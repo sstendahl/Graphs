2ce6a3ac5432211f42b4dc7448117bc18c944e28
Sjoerd Broekhuijsen <sjoerd1993@gmail.com>
2022-12-15 08:16:14 +0100

    Fixed regression bugs
    Fixed a bug where powers where not possible in the equation editor
    Fixed a bug where duplicate filenames were not detected if the user kept the filename blank.

--- src/add_equation.py
+++ src/add_equation.py
@@ -16,8 +16,8 @@ def on_accept(widget, self, window):
     x_stop = float(window.X_stop_entry.get_text())
     step_size = float(window.step_size_entry.get_text())
     equation = str(window.equation_entry.get_text())
-    name = str(window.name_entry.get_text())
-    new_file = create_data(self, x_start, x_stop, equation, step_size, name)
+    new_file = create_data(self, x_start, x_stop, equation, step_size, str(window.name_entry.get_text()))
+    name = new_file.filename
     if name in self.datadict:
         loop = True
         i = 0
@@ -28,8 +28,9 @@ def on_accept(widget, self, window):
                 loop = False
                 new_file.filename = f"{name} ({i})"
     color = plotting_tools.get_next_color(self)
-    datman.add_sample_to_menu(self, new_file.filename, color)
     self.datadict[new_file.filename] = new_file
+    datman.add_sample_to_menu(self, new_file.filename, color)
+    datman.select_top_row(self)
 
     plotting_tools.refresh_plot(self)
     window.destroy()
@@ -39,10 +40,11 @@ def create_data(self, x_start, x_stop, equation, step_size, name):
     if name == "":
         name = f"Y = {str(equation)}"
     datapoints = int(abs(x_start - x_stop)/step_size)
-    new_file.xdata =  ndarray.tolist(linspace(x_start,x_stop,datapoints))    
+    new_file.xdata =  linspace(x_start,x_stop,datapoints)
     equation = equation.replace("X", "new_file.xdata")
-    equation = equation.replace("^", "**")
+    equation = str(equation.replace("^", "**"))
     new_file.ydata = eval(equation)
+    new_file.xdata = ndarray.tolist(new_file.xdata)
     new_file.filename = name
     return new_file
 
@@ -59,12 +61,11 @@ class AddEquationWindow(Adw.Window):
 
     def __init__(self, parent):
         super().__init__()
+        self.set_transient_for=(parent.props.active_window)
+        self.props.modal = True
         buffer = Gtk.TextBuffer()
         text1 = "Here you can add add a data by equation\n"
         text2 = "The equation field uses Numpy notation \n"
         text3 = "Make sure to use a capital letter for the X coordinate"
         buffer.set_text(text1 + text2 + text3)
         self.equation_info.set_buffer(buffer)
-
-
-
