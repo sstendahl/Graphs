c97a7d2617ab70af89c9751505a4799ed55e1b20
Sjoerd Broekhuijsen <sjoerd1993@gmail.com>
2023-01-20 13:24:17 +0100

    Update preferences.py

--- src/preferences.py
+++ src/preferences.py
@@ -38,8 +38,8 @@ class Preferences():
     def create_new_config_file(self):
         config_path = self.get_config_path()
         if not os.path.isfile(f"{config_path}/config.json"):
-            print("New configuration file created")
             self.reset_config()    
+            print("New configuration file created")
         else:
             print("Loading configuration file")
 
