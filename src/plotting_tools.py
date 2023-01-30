a4118c338d24e491d143a0e86973d7b596090e7d
Sjoerd Broekhuijsen <sjoerd1993@gmail.com>
2023-01-30 07:38:05 +0100

    Disable coordinates + docstrings

--- src/plotting_tools.py
+++ src/plotting_tools.py
@@ -15,9 +15,15 @@ import matplotlib.font_manager
 import time 
 
 def define_highlight(self, span=None):
+    """
+    Create a span selector object, to highlight part of the graph.
+    If a span already exists, make it visible instead
+    """
+    
+    #This lamdba function is not pretty, but it doesn't accept a "Pass"
     self.highlight = SpanSelector(
         self.canvas.top_right_axis,
-        on_select,
+        lambda x, y: x,
         "horizontal",
         useblit=True,
         props=dict(alpha=0.3, facecolor = "tab:blue", linewidth = 0),
@@ -29,10 +35,11 @@ def define_highlight(self, span=None):
         self.highlight.set_visible(True)
         self.highlight.set_active(True)
 
-def on_select(self, xmin):
-    pass
-
 def hide_highlight(self):
+    """
+    Hide the SpanSelector object from the graph, and disable the select data
+    button
+    """
     win = self.props.active_window
     button = win.select_data_button
     self.highlight.set_visible(False)
@@ -40,6 +47,9 @@ def hide_highlight(self):
     button.set_active(False)
 
 def toggle_highlight(shortcut, _, self):
+    """
+    Toggle the SpanSelector. 
+    """
     win = self.props.active_window
     button = win.select_data_button
     if self.highlight == None:
@@ -55,6 +65,11 @@ def toggle_highlight(shortcut, _, self):
 
 def plot_figure(self, canvas, X, Y, filename="", xlim=None, linewidth = 2, title="", scale="log",marker=None, linestyle="solid",
                      revert = False, color = None, marker_size = 10, y_axis = "left", x_axis = "bottom"):
+    """
+    Plot the figure on the graph
+    Necessary input arguments are self, the canvas to plot the figure on and the
+    X and Y data
+    """
     if y_axis == "left":
         if x_axis == "bottom":
             canvas.ax.plot(X, Y, linewidth = linewidth ,label=filename, linestyle=linestyle, marker=marker, color = color, markersize=marker_size)
@@ -68,6 +83,9 @@ def plot_figure(self, canvas, X, Y, filename="", xlim=None, linewidth = 2, title
     set_legend(self, canvas)
 
 def set_legend(self, canvas):
+    """
+    Set the legend of the graph
+    """
     if self.plot_settings.legend:
         canvas.legends = []
         lines, labels = canvas.ax.get_legend_handles_labels()
@@ -77,6 +95,9 @@ def set_legend(self, canvas):
         canvas.top_right_axis.legend(lines + lines2 + lines3 + lines4, labels + labels2 + labels3 + labels4, loc=0)
 
 def set_canvas_limits_axis(self, canvas, limits = {"xmin":None, "xmax":None, "ymin":None, "ymax":None}):
+    """
+    Set the canvas limits for each axis that is present
+    """
     left_datadict = dict()
     right_datadict = dict()
     top_datadict = dict()
@@ -89,9 +110,15 @@ def set_canvas_limits_axis(self, canvas, limits = {"xmin":None, "xmax":None, "ym
             set_canvas_limits(self, graph_limits, axis)
             
 def set_canvas_limits(self, graph_limits, axis, limits = {"xmin":None, "xmax":None, "ymin":None, "ymax":None}):
+    """
+    Set an calculate the canvas limits for a given axis. 
+    """
+    
+    #Update graph limits with limits that were given as argument
     for key, item in limits.items():
         if item is not None:
             graph_limits[key] = item
+            
     x_span = (graph_limits["xmax"] - graph_limits["xmin"])
     y_span = (graph_limits["ymax"] - graph_limits["ymin"]) 
     if axis.get_xscale() == "linear":
@@ -117,10 +144,15 @@ def set_canvas_limits(self, graph_limits, axis, limits = {"xmin":None, "xmax":No
         print("Could not set limits, one of the values was probably infinite")
         
 def find_limits(self, axis, canvas, datadict):
+    """
+    Find the limits that are to be used for the axes.
+    """
     xmin_all = None
     xmax_all = None
     ymin_all = None
     ymax_all = None
+    
+    #Check which xaxis and yaxis are being used based on the axis given
     if axis == canvas.ax:
         xaxis = "bottom"
         yaxis = "left"
@@ -135,7 +167,9 @@ def find_limits(self, axis, canvas, datadict):
         yaxis = "right"    
 
     for key, item in datadict.items():
+        #Check the limits of each item, as long as it exists and it has the same axes as the one we're adjusting right now
         if item is not None and len(item.xdata) > 0 and item.plot_Y_position == yaxis and item.plot_X_position == xaxis:
+            #Nonzero ydata is needed for logs
             nonzero_ydata = list(filter(lambda x: (x != 0), item.ydata))
             xmin_item = min(item.xdata)
             xmax_item = max(item.xdata)
@@ -166,6 +200,9 @@ def find_limits(self, axis, canvas, datadict):
 
 
 def reload_plot(self, from_dictionary = True):
+    """
+    Completely reload the plot of the graph
+    """
     win = self.props.active_window
     graphs.clear_layout(self)
     graphs.load_empty(self)
@@ -179,6 +216,9 @@ def reload_plot(self, from_dictionary = True):
 
 
 def refresh_plot(self, canvas = None, from_dictionary = True, set_limits = True):
+    """
+    Refresh the graph without completely reloading it. 
+    """
     if canvas == None:
         canvas = self.canvas
     for line in canvas.ax.lines:
@@ -197,6 +237,9 @@ def refresh_plot(self, canvas = None, from_dictionary = True, set_limits = True)
     self.canvas.draw()
 
 def hide_unused_axes(self, canvas):
+    """
+    Hide axes that are not in use, to avoid unnecessary ticks in the plots.
+    """
     #Double check the code here, seems to work but this is too messy
     for axis in [canvas.ax, canvas.right_axis, canvas.top_left_axis, canvas.top_right_axis]:
         axis.get_xaxis().set_visible(True)
@@ -234,6 +277,9 @@ def hide_unused_axes(self, canvas):
 
     
 def get_next_color(self):
+    """
+    Get the color that is to be used for the next data set
+    """
     color_list = self.canvas.color_cycle
     used_colors = []
     item_rows = copy.copy(self.item_rows)
@@ -249,6 +295,9 @@ def get_next_color(self):
             return color
 
 def load_fonts(self):
+    """
+    Load system fonts that are installed on the system
+    """
     font_list = matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
     for font in font_list:
         try:
@@ -257,6 +306,10 @@ def load_fonts(self):
             print(f"Could not load {font}")
             
 class PlotSettings:
+    """
+    The plot-related settings for the current session. The default values are 
+    retreived from the config file through preferences.
+    """
     def __init__(self, parent):
         self.font_string = parent.preferences.config["plot_font_string"]
         self.xlabel = parent.preferences.config["plot_X_label"]
@@ -289,6 +342,9 @@ class PlotSettings:
 
         
 class PlotWidget(FigureCanvas):
+    """
+    Create the widget that contains the graph itself 
+    """
     def __init__(self, parent=None, xlabel="", ylabel="", yscale = "log", title="", scale="linear", style = "seaborn-whitegrid"):
         self.figure = Figure()
         self.figure.set_tight_layout(True)
@@ -302,17 +358,30 @@ class PlotWidget(FigureCanvas):
         self.right_axis = self.ax.twinx()
         self.top_left_axis = self.ax.twiny()
         self.top_right_axis = self.top_left_axis.twinx()
+        
+        #Set the coordinates in the bottom-right corner as an empty string
+        #These only work for the top-right axis anyway, so is broken in 95% of 
+        #the cases, and makes the experience very bad for small window sizes.
+        #This feature should be implemented differently perhaps.
+        self.top_right_axis.format_coord = lambda x, y: ""
         self.set_ax_properties(parent)
         self.set_save_properties(parent)
         self.set_color_cycle(parent)
         super(PlotWidget, self).__init__(self.figure)
 
     def set_save_properties(self, parent):
+        """
+        Set the properties that are related to saving the figure. Currently
+        limited to savefig, but will include the background colour soon.
+        """
         plt.rcParams["savefig.format"] = parent.preferences.config["savefig_filetype"]
         if parent.preferences.config["savefig_transparent"]:
             plt.rcParams["savefig.transparent"] = True
 
     def set_ax_properties(self, parent):
+        """
+        Set the properties that are related to the axes. 
+        """
         self.title = self.ax.set_title(parent.plot_settings.title)
         self.bottom_label = self.ax.set_xlabel(parent.plot_settings.xlabel, fontweight = parent.plot_settings.font_weight)
         self.right_label = self.right_axis.set_ylabel(parent.plot_settings.right_label, fontweight = parent.plot_settings.font_weight)
@@ -326,6 +395,9 @@ class PlotWidget(FigureCanvas):
         self.set_ticks(parent)
 
     def set_ticks(self, parent):
+        """
+        Set the ticks that are to be used in the graph. 
+        """
         for axis in [self.top_right_axis, self.top_left_axis, self.ax, self.right_axis]:
             axis.tick_params(direction=parent.plot_settings.tick_direction, length=parent.plot_settings.major_tick_length, width=parent.plot_settings.major_tick_width, which="major")
             axis.tick_params(direction=parent.plot_settings.tick_direction, length=parent.plot_settings.minor_tick_length, width=parent.plot_settings.minor_tick_width, which="minor")
@@ -351,6 +423,9 @@ class PlotWidget(FigureCanvas):
                 axis.tick_params(which = "both", left=parent.plot_settings.tick_left, right=parent.plot_settings.tick_right)
 
     def set_style(self, parent):
+        """
+        Set the plot style. 
+        """
         plt.rcParams.update(plt.rcParamsDefault)
         if Adw.StyleManager.get_default().get_dark():
             self.figure.patch.set_facecolor("#242424")
@@ -381,6 +456,9 @@ class PlotWidget(FigureCanvas):
         plt.rcParams.update(params)
 
     def set_color_cycle(self, parent):
+        """
+        Set the color cycle that will be used for the graphs. 
+        """
         cmap = parent.preferences.config["plot_color_cycle"]
         reverse_dark = parent.preferences.config["plot_invert_color_cycle_dark"]
         if Adw.StyleManager.get_default().get_dark() and reverse_dark:
@@ -389,6 +467,15 @@ class PlotWidget(FigureCanvas):
         self.color_cycle = color_cycle.by_key()['color']
 
     def __call__(self, event):
+        """
+        The function is called when a user clicks on it. 
+        If two clicks are performed close to each other, it registers as a double
+        click, and if these were on a specific item (e.g. the title) it triggers
+        a dialog to edit this item.
+        
+        Unfortunately the GTK Doubleclick signal doesn't work with matplotlib
+        hence this custom function.
+        """
         double_click = False
         if self.one_click_trigger == False:
             self.one_click_trigger = True
@@ -413,6 +500,3 @@ class PlotWidget(FigureCanvas):
             rename_label.open_rename_label_window(self.parent, self.left_label)
         if self.right_label.contains(event)[0] and double_click:
             rename_label.open_rename_label_window(self.parent, self.right_label)
-
-
-
