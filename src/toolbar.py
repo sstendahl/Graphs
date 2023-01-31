85e604c720e8a642c264122ed9a7dd0f0a327472
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2023-01-31 16:14:27 +0100

    small toolbar change
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- src/toolbar.py
+++ src/toolbar.py
@@ -6,10 +6,12 @@ def pan(widget, shortcut, self):
     plotting_tools.set_mode(self, self.dummy_toolbar.mode)
 
 def view_back(widget, shortcut, self):
-    self.dummy_toolbar.back()
+    self.dummy_toolbar._nav_stack.back()
+    self.dummy_toolbar._update_view()
 
 def view_forward(widget, shortcut, self):
-    self.dummy_toolbar.forward()
+    self.dummy_toolbar._nav_stack.forward()
+    self.dummy_toolbar._update_view()
 
 def zoom(widget, shortcut, self):
     self.dummy_toolbar.zoom()
