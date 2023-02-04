d6147f6502e7648a0b08c2a7558eebc94da22c76
Sjoerd Broekhuijsen <sjoerd1993@gmail.com>
2023-02-04 21:38:44 +0100

    Inherit colorpicker from GtkButton

--- src/colorpicker.py
+++ src/colorpicker.py
@@ -1,25 +1,61 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
-from gi.repository import Gtk
+import gi
+from . import plotting_tools, utilities
+from gi.repository import Gtk, GObject, Gdk
 from matplotlib import colors
 
-class ColorPicker(Gtk.ColorButton):
+class ColorPicker(Gtk.Button):
     def __init__(self, color, row, parent):
         super().__init__()
+        #self.set_size_request(45, -1)
+        self.row = row
+        self.parent = parent
         self.set_tooltip_text(_("Pick a color"))
-        self.provider = Gtk.CssProvider()
-        self.set_size_request(10, 15)
-        rgba = utilities.create_rgba(*colors.to_rgba(color))
-        self.set_rgba(rgba)
-        self.connect("color-set", self.on_color_set, parent)
-        style_context = self.get_style_context()        
-        style_context.add_class("circular")                
         self.color = color
-        self.update_color()
+        style_context = self.get_style_context()
+        style_context.add_class("circular")
+
+        press_gesture = Gtk.GestureClick()
+        press_gesture.connect("pressed", self.change_color)
+        
+        self.color_chooser = Gtk.ColorChooserWidget.new()
+        self.color_chooser.set_use_alpha(False)
+        self.color_chooser.show()
+        self.color_chooser.connect("color-activated", self.change_color)        
+        self.color_chooser.add_controller(press_gesture)
+
+        self.color_popover = Gtk.Popover()
+        self.color_popover.set_parent(self)
+        self.color_popover.set_child(self.color_chooser)
+        self.color_popover.connect("closed", self.change_color)
+        self.connect("clicked", self.on_click)
+        self.set_css()
 
     def on_color_set(self, widget, graphs):
         self.color = self.get_color()
         self.update_color()
         plotting_tools.refresh_plot(graphs)
+
+    def set_rgba(self, color):
+        self.color_chooser.set_rgba(color)
+        self.update_color()
+
+    def on_click(self, button):
+        self.popover_active = True
+        self.color_chooser.props.show_editor = False
+        self.color_popover.popup()
+
+    def set_color(self, chooser, color, _ = None):
+        self.set_rgba(color)
+        self.color = self.get_color()
+        self.update_color()
+        plotting_tools.refresh_plot(self.parent)
+
+    def change_color(self, *args):
+        self.set_color(self.color_chooser, self.get_rgba())
+
+    def get_rgba(self):
+        return self.color_chooser.get_rgba()
 
     def convert_rgba(self, rgba):
         return (round(rgba.red*255), round(rgba.green*255), round(rgba.blue*255), round(rgba.alpha*255))
@@ -30,22 +66,14 @@ class ColorPicker(Gtk.ColorButton):
         return color_hex
 
     def update_color(self):
-        color = self.get_color()
-        self.set_css_classes([f"button_{color[1:]}", "flat"])
-        css = f'''
-                 .button_{color[1:]} {{
-                      color: {color};
-                 }}
-                '''
-        self.set_css(css)
+        css = f'button {{ background: {self.get_rgba().to_string()}; }}'
+        self.provider.load_from_data(css.encode())
 
-
-    def set_css(self, css):
-            css_provider = Gtk.CssProvider()
-            css_provider.load_from_data(css.encode())
-            context = self.get_style_context()
-            context.add_class("circular")    
-            display = self.get_display()
-            self.provider.load_from_data(css.encode())
-            context.add_provider_for_display(display, css_provider,
-                                             Gtk.STYLE_PROVIDER_PRIORITY_USER)
+    def set_css(self):
+        self.provider = Gtk.CssProvider()
+        context = self.get_style_context()
+        context.add_provider(self.provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+        rgba = utilities.create_rgba(*colors.to_rgba(self.color))
+        self.set_rgba(rgba)
+        self.set_rgba(self.get_rgba())
+        self.update_color()
