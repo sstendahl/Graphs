ea6ec308078a7b6ed20d7e8b1650bb27de9f2229
Sjoerd Broekhuijsen <sjoerd1993@gmail.com>
2023-02-06 21:11:55 +0100

    reduce redundant plot refreshes
    Major performance improvement when opening multiple data. It used to select each data using the regular select_function when first plotting, which triggers a refresh-plot. So when importing 10 data sets, it refreshes the plot 10 unnecessary times. This is now prevented.

--- src/graphs.py
+++ src/graphs.py
@@ -3,10 +3,10 @@ import gi
 import os
 import re
 import numpy
+import pickle
 
 from gi.repository import Gtk, Adw
 from matplotlib.backends.backend_gtk4 import NavigationToolbar2GTK4 as NavigationToolbar
-
 from . import plotting_tools, samplerow, colorpicker, utilities
 from .plotting_tools import PlotWidget
 from .data import Data
@@ -21,21 +21,21 @@ def get_theme_color(self):
 
 def open_selection_from_dict(self):
     for key, item in self.datadict.items():
-        if item is not None:
-            if self.item_rows[key].check_button.get_active():
-                linewidth = item.selected_line_thickness
-                linestyle = item.linestyle_selected
-                marker = item.selected_markers
-                marker_size = item.selected_marker_size
-            else:
-                linewidth = item.unselected_line_thickness
-                linestyle = item.linestyle_unselected
-                marker = item.unselected_markers
-                marker_size = item.unselected_marker_size
-            color = self.item_rows[key].color_picker.color
-            y_axis = item.plot_Y_position
-            x_axis = item.plot_X_position
-            plotting_tools.plot_figure(self, self.canvas, item.xdata,item.ydata, item.filename, linewidth = linewidth, linestyle=linestyle, color = color, marker = marker, marker_size = marker_size, y_axis = y_axis, x_axis = x_axis)
+        if self.item_rows[key].check_button.get_active():
+            linewidth = item.selected_line_thickness
+            linestyle = item.linestyle_selected
+            marker = item.selected_markers
+            marker_size = item.selected_marker_size
+        else:
+            linewidth = item.unselected_line_thickness
+            linestyle = item.linestyle_unselected
+            marker = item.unselected_markers
+            marker_size = item.unselected_marker_size
+        color = self.item_rows[key].color_picker.color
+        y_axis = item.plot_Y_position
+        x_axis = item.plot_X_position
+        print("Opening from dict")
+        plotting_tools.plot_figure(self, self.canvas, item.xdata,item.ydata, item.filename, linewidth = linewidth, linestyle=linestyle, color = color, marker = marker, marker_size = marker_size, y_axis = y_axis, x_axis = x_axis)
 
 def open_selection_from_file(self, files, import_settings):
     for path in files:
@@ -73,8 +73,7 @@ def open_selection_from_file(self, files, import_settings):
                 item.color = plotting_tools.get_next_color(self)
                 plotting_tools.plot_figure(self, self.canvas, item.xdata,item.ydata, item.filename, item.color, y_axis = y_axis, x_axis = x_axis)
                 add_sample_to_menu(self, item.filename, item.color, item.id, select_item = True)
-                select_item(self, item.id)
-                plotting_tools.reload_plot(self)
+    plotting_tools.reload_plot(self)
     self.canvas.draw()
     plotting_tools.set_canvas_limits_axis(self, self.canvas)
 
@@ -183,7 +182,7 @@ def delete_selected(shortcut, _,  self):
 
 def delete(widget,  self, id, give_toast = True):
     layout = self.list_box
-    for key, item in self.sample_menu.items():
+    for key, item in self.data_list.items():
         if key == id:
             layout.remove(item)
     filename = self.datadict[id].filename
@@ -221,11 +220,13 @@ def add_sample_to_menu(self, filename, color, id, select_item = False):
     self.list_box = win.list_box
     row = samplerow.SampleBox(self, filename, id)
     row.gesture.connect("released", row.clicked, self)
-    row.color_picker = colorpicker.ColorPicker(color, row=row, parent=self)
+    row.color_picker = colorpicker.ColorPicker(color, key=id, parent=self)
     row.color_picker.set_hexpand(False)
     label = row.sample_ID_label
     row.sample_box.insert_child_after(row.color_picker, row.sample_ID_label)
     row.check_button.connect("toggled", toggle_data, self, id)
+    if select_item:
+        row.check_button.set_active(True)
     row.delete_button.connect("clicked", delete, self, id)
     max_length = int(26)
     if len(filename) > max_length:
@@ -235,7 +236,7 @@ def add_sample_to_menu(self, filename, color, id, select_item = False):
     row.sample_ID_label.set_text(label)
     self.list_box.append(row)
     self.item_rows[id] = row
-    self.sample_menu[id] = self.list_box.get_last_child()
+    self.data_list[id] = self.list_box.get_last_child()
 
 def toggle_data(widget,  self, id):
     plotting_tools.refresh_plot(self)
@@ -267,11 +268,21 @@ def save_file_dialog(self, documenttype="Text file (*.txt)"):
         self.props.active_window.toast_overlay.add_toast(Adw.Toast(title=f"Could not open save dialog, make sure you have data opened"))
 
 
-def on_save_response(dialog, response, self):
+def on_save_response(dialog, response, self, project = False):
     files = []
     if response == Gtk.ResponseType.ACCEPT:
         path = dialog.get_file().peek_path()
-        save_file(self, path)
+        if project:
+            save_project_file(self, path)
+        else:
+            save_file(self, path)
+
+def save_project_file(self, path):
+    project_data = dict()
+    project_data["plot_settings"] = self.plot_settings
+    project_data["data"] = self.datadict
+    with open(path, 'wb') as f:
+        pickle.dump(project_data, f)
 
 
 def save_file(self, path):
@@ -294,7 +305,23 @@ def save_file(self, path):
                 numpy.savetxt(str(path + "/" + filename) + ".txt", array, delimiter="\t")
 
 
-def open_file_dialog(widget, _, self, import_settings = None):
+def save_project_dialog(widget, _, self, documenttype="Graphs Project (*)"):
+    def save_project_chooser(action):
+        dialog = Gtk.FileChooserNative.new(
+            title="Save files",
+            parent=self.props.active_window,
+            action=action,
+            accept_label="_Save",
+        )
+        return dialog
+
+    chooser = save_project_chooser(Gtk.FileChooserAction.SAVE)
+    chooser.set_modal(True)
+    chooser.connect("response", on_save_response, self, True)
+    chooser.show()
+
+
+def open_file_dialog(widget, _, self, import_settings = None, open_project = False):
     open_file_chooser = Gtk.FileChooserNative.new(
         title="Open new files",
         parent=self.props.active_window,
@@ -303,8 +330,11 @@ def open_file_dialog(widget, _, self, import_settings = None):
     )
 
     open_file_chooser.set_modal(True)
-    open_file_chooser.set_select_multiple(True)
-    open_file_chooser.connect("response", on_open_response, self, import_settings)
+    if open_project:
+        open_file_chooser.connect("response", load_project, self)
+    else:
+        open_file_chooser.set_select_multiple(True)
+        open_file_chooser.connect("response", open_file, self, import_settings)
     open_file_chooser.show()
 
 def get_import_settings(self):
@@ -317,8 +347,30 @@ def get_import_settings(self):
     import_settings["column_y"] = int(self.preferences.config["import_column_y"])
     import_settings["name"] = ""
     return import_settings
+    
+def load_project(dialog, response, self):
+    files = []
+    if response == Gtk.ResponseType.ACCEPT:
+        for file in dialog.get_files():
+            file_path = file.peek_path()
+            filename = file_path.split("/")[-1]
+            files.append(file_path)
+    select_all(None, None, self)
+    delete_selected(None, None, self)
 
-def on_open_response(dialog, response, self, import_settings):
+    with open(file_path, 'rb') as f:
+        project =  pickle.load(f)
+    self.datadict = dict()
+    self.datadict = project["data"]
+    self.plot_settings = project["plot_settings"]
+    for key, item in self.datadict.items():
+        color = item.color
+        add_sample_to_menu(self, item.filename, color, item.id)
+    plotting_tools.reload_plot(self)
+    select_all(None, None, self)
+
+
+def open_file(dialog, response, self, import_settings):
     files = []
     if response == Gtk.ResponseType.ACCEPT:
         for file in dialog.get_files():
@@ -332,7 +384,7 @@ def on_open_response(dialog, response, self, import_settings):
             import_settings["mode"] = "multiple"
         elif len(dialog.get_files()) == 1:
             import_settings["mode"] = "single"
-
+        print("Opening from file")
         open_selection_from_file(self, files, import_settings)
 
 
@@ -378,4 +430,3 @@ def enable_data_dependent_buttons(self, enabled):
 
     for button in dependent_buttons:
         button.set_sensitive(enabled)
-
