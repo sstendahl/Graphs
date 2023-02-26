114040b30609aa3901ffb6a1f53f794406bf130c
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2023-02-26 22:33:22 +0100

    apply spdx-identifier
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- src/calculation.py
+++ src/calculation.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
 import numpy
 from numpy import *
 
--- src/operation_tools.py
+++ src/operation_tools.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
 from graphs.data import Data
 
 import numpy
