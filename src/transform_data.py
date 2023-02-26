2855d516cb4413d9bbd772eef6674a60e913dfc0
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2023-02-26 22:51:34 +0100

    fix transform window
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- src/transform_data.py
+++ src/transform_data.py
@@ -3,7 +3,7 @@ import logging
 
 from gi.repository import Adw, Gtk
 
-from graphs import calculation, operation_tools
+from graphs import calculation, operation_tools, operations
 from graphs import clipboard, plotting_tools, utilities
 from graphs.misc import InteractionMode
 
@@ -26,48 +26,34 @@ class TransformWindow(Adw.Window):
     def accept(self, _widget, parent):
         input_x = str(self.transform_x_entry.get_text())
         input_y = str(self.transform_y_entry.get_text())
-        selected_keys = utilities.get_selected_keys(parent)
-        if parent.interaction_mode == InteractionMode.SELECT:
-            _selection, start_stop = operation_tools.select_data(parent)
-
-        for key in selected_keys:
-            if f"{key}_selected" in parent.datadict:
-                start_index = start_stop[key][0]
-                stop_index = start_stop[key][1]
-                xdata_in = parent.datadict[key].xdata[start_index:stop_index]
-                try:
-                    xdata_out, ydata_out = calculation.operation(
-                        xdata_in, input_x, input_y)
-                except Exception as exception:
-                    exception_type = exception.__class__.__name__
-                    toast = f"{exception_type}: Unable to do transformation, "
-                    + "make sure the syntax is correct"
-                    parent.main_window.add_toast(toast)
-                    logging.exception("Unable to do transformation")
-                    return
-                parent.datadict[key].xdata[start_index:stop_index] = xdata_out
-                parent.datadict[key].ydata[start_index:stop_index] = ydata_out
-            if parent.interaction_mode != InteractionMode.SELECT:
-                xdata_in = parent.datadict[key].xdata
-                ydata_in = parent.datadict[key].ydata
-                try:
-                    xdata_out, ydata_out = calculation.operation(
-                        xdata_in, ydata_in, input_x, input_y)
-                except Exception as exception:
-                    exception_type = exception.__class__.__name__
-                    toast = f"{exception_type}: Unable to do transformation, \
+        keys = utilities.get_selected_keys(parent)
+        select_mode = parent.interaction_mode == InteractionMode.SELECT
+        if select_mode:
+            start_stop = operation_tools.select_data(parent, keys)
+        for key in keys:
+            item = operations.get_item(parent, key)
+            if select_mode:
+                start_index, stop_index = \
+                    start_stop[key][0], start_stop[key][1]
+            else:
+                start_index, stop_index = [0, len(item.xdata)]
+            try:
+                xdata_out, ydata_out = calculation.operation(
+                    item.xdata, item.ydata,
+                    input_x, input_y)
+            except Exception as exception:
+                exception_type = exception.__class__.__name__
+                toast = f"{exception_type}: Unable to do transformation, \
 make sure the syntax is correct"
-                    parent.main_window.add_toast(toast)
-                    logging.exception("Unable to do transformation")
-                    return
-                parent.datadict[key].xdata = xdata_out
-                parent.datadict[key].ydata = ydata_out
-            parent.datadict[key].xdata, parent.datadict[
-                key].ydata = operation_tools.sort_data(
+                parent.main_window.add_toast(toast)
+                logging.exception("Unable to do transformation")
+                return
+            parent.datadict[key].xdata[start_index:stop_index] = xdata_out
+            parent.datadict[key].ydata[start_index:stop_index] = ydata_out
+            parent.datadict[key].xdata, parent.datadict[key].ydata = \
+                operation_tools.sort_data(
                     parent.datadict[key].xdata, parent.datadict[key].ydata)
-        for key in parent.datadict.copy():
-            if key.endswith("_selected"):
-                del (parent.datadict[key])
+        operation_tools.delete_selected(parent)
         clipboard.add(parent)
         plotting_tools.refresh_plot(parent)
         self.destroy()
