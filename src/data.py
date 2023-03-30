705b26c19e3d9d1a15304061d8b560b1b3b2b5e7
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2023-03-30 21:28:09 +0200

    inline config query
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- src/data.py
+++ src/data.py
@@ -7,30 +7,6 @@ from graphs.misc import ImportMode, ImportSettings
 
 class Data:
     def __init__(self, parent, xdata, ydata, import_settings=None):
-        self.filename = ""
-        self.linestyle_selected = ""
-        self.linestyle_unselected = ""
-        self.selected_line_thickness = float(0)
-        self.unselected_line_thickness = float(0)
-        self.selected_markers = ""
-        self.unselected_markers = ""
-        self.selected_marker_size = float(0)
-        self.unselected_marker_size = float(0)
-        self.plot_y_position = "left"
-        self.plot_x_position = "bottom"
-        self.selected = True
-        self.color = ""
-        self.xdata = xdata
-        self.ydata = ydata
-        self.clipboard_pos = -1
-        self.xdata_clipboard = [self.xdata.copy()]
-        self.ydata_clipboard = [self.ydata.copy()]
-        self.key: str = str(uuid.uuid4())
-        if import_settings is None:
-            import_settings = ImportSettings(parent)
-        self.set_data_properties(parent, import_settings)
-
-    def set_data_properties(self, parent, import_settings):
         cfg = parent.preferences.config
         self.plot_y_position = cfg["plot_y_position"]
         self.plot_x_position = cfg["plot_x_position"]
@@ -42,6 +18,18 @@ class Data:
         self.unselected_markers = cfg["plot_unselected_markers"]
         self.selected_marker_size = cfg["plot_selected_marker_size"]
         self.unselected_marker_size = cfg["plot_unselected_marker_size"]
+        self.selected_marker_size = float(0)
+        self.unselected_marker_size = float(0)
+        self.selected = True
+        self.color = ""
+        self.xdata = xdata
+        self.ydata = ydata
+        self.clipboard_pos = -1
+        self.xdata_clipboard = [self.xdata.copy()]
+        self.ydata_clipboard = [self.ydata.copy()]
+        self.key: str = str(uuid.uuid4())
+        if import_settings is None:
+            import_settings = ImportSettings(parent)
         if import_settings.name != "" \
                 and import_settings.mode == ImportMode.SINGLE:
             filename = import_settings.name
