01f43c542cbc886bf68f10842390c7f2debf7f45
SjoerdB93 <sjoerd1993@gmail.com>
2023-04-02 14:32:07 +0200

    Fix linting

--- src/utilities.py
+++ src/utilities.py
@@ -13,13 +13,12 @@ def remove_unused_config_keys(config, template):
         del config[key]
     return config
 
+
 def sig_fig_round(number, digits):
-    """
-    Round a number to the specified number of significant digits.
-    """
-    if number == None:
+    """Round a number to the specified number of significant digits."""
+    if number is None:
         return None
-    power = "{:e}".format(float(number)).split('e')[1]
+    power = "{:e}".format(float(number)).split("e")[1]
     return round(float(number), -(int(power) - digits + 1))
 
 
