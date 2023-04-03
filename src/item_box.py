f553eae1572c380d98eea12ee9b33ef24c77f19f
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2023-04-03 15:08:21 +0200

    fix linting
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- src/item_box.py
+++ src/item_box.py
@@ -50,7 +50,7 @@ class ItemBox(Gtk.Box):
             self.parent.item_boxes, drop_target.key, value)
         self.parent.item_menu = utilities.change_key_position(
             self.parent.item_menu, drop_target.key, value)
-        for key, item in self.parent.item_menu.items():
+        for _key, item in self.parent.item_menu.items():
             self.parent.main_window.list_box.remove(item)
             self.parent.main_window.list_box.append(item)
         graphs.reload(self.parent)
