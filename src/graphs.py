71eea25cf2a3cc8ff1b2fe95f593a1831b30d2db
SjoerdB93 <sjoerd1993@gmail.com>
2023-04-03 11:35:13 +0200

    Revert plot order
    Reverses the plot order so that the top item actually gets plotted on top

--- src/graphs.py
+++ src/graphs.py
@@ -143,7 +143,7 @@ def refresh(self, set_limits=False):
         line.remove()
     if len(self.datadict) > 0:
         plotting_tools.hide_unused_axes(self, canvas)
-    for _key, item in self.datadict.items():
+    for _key, item in reversed(self.datadict.items()):
         if item is not None:
             self.canvas.plot(item, item.selected)
     if set_limits and len(self.datadict) > 0:
