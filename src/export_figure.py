c5b71c276e70bc0be5f681eda2f46dbb15a0277f
SjoerdB93 <sjoerd1993@gmail.com>
2023-04-11 08:22:32 +0200

    Add dpi option for figure export

--- src/export_figure.py
+++ src/export_figure.py
@@ -12,6 +12,7 @@ class ExportFigureWindow(Adw.Window):
     confirm_button = Gtk.Template.Child()
     file_format = Gtk.Template.Child()
     transparent_switch = Gtk.Template.Child()
+    export_figure_dpi = Gtk.Template.Child()
 
     def __init__(self, parent):
         super().__init__()
@@ -21,11 +22,14 @@ class ExportFigureWindow(Adw.Window):
         self.transparent_switch.set_active(
             self.parent.preferences.config["export_figure_transparent"])
         items = self.parent.canvas.get_supported_filetypes_grouped().items()
+        self.export_figure_dpi.set_value(
+            int(self.parent.preferences.config["export_figure_dpi"]))
         file_formats = []
         default_format = None
         for name, formats in items:
             file_formats.append(name)
-            if self.parent.preferences.config["savefig_filetype"] in formats:
+            if self.parent.preferences.config["export_figure_filetype"] in \
+                formats:
                 default_format = name
         utilities.populate_chooser(self.file_format, file_formats)
         if default_format is not None:
@@ -33,6 +37,7 @@ class ExportFigureWindow(Adw.Window):
         self.present()
 
     def on_accept(self, _):
+        dpi = int(self.export_figure_dpi.get_value())
         fmt = self.file_format.get_selected_item().get_string()
         file_suffix = None
         items = self.parent.canvas.get_supported_filetypes_grouped().items()
@@ -45,17 +50,17 @@ class ExportFigureWindow(Adw.Window):
         dialog.set_initial_name(f"{filename}.{file_suffix}")
         dialog.set_accept_label("Export")
         dialog.save(
-            self.parent.main_window, None, self.on_figure_save_response,
+            self.parent.main_window, None, self.on_figure_save_response, dpi,
             file_suffix, transparent)
         self.destroy()
 
     def on_figure_save_response(
-            self, dialog, response, file_suffix, transparent):
+            self, dialog, response, dpi, file_suffix, transparent):
         try:
             path = dialog.save_finish(response).get_path()
             if path is not None:
                 self.parent.canvas.figure.savefig(
-                    path, format=file_suffix, transparent=transparent)
+                    path, dpi=dpi, format=file_suffix, transparent=transparent)
                 self.parent.main_window.add_toast("Exported Figure")
         except GLib.GError:
             pass
