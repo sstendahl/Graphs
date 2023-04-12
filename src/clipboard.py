6ba8378dd9f297f755fd431afbd390f9222d5150
SjoerdB93 <sjoerd1993@gmail.com>
2023-04-12 12:02:24 +0200

    Fix clipboard size check

--- src/clipboard.py
+++ src/clipboard.py
@@ -21,7 +21,7 @@ def add(self):
     self.clipboard_pos = -1
     self.datadict_clipboard.append(copy.deepcopy(self.datadict))
     if len(self.datadict_clipboard) > \
-            self.preferences.config["clipboard_length"] + 1:
+            int(self.preferences.config["clipboard_length"]) + 1:
         self.datadict_clipboard = \
             self.datadict_clipboard[1:]
 
