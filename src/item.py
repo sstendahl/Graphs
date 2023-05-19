7eb536a4dff7f002a8f4b99bbf1fa475fb673603
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2023-05-19 11:40:14 +0200

    WIP: Multiple Item types.
    First steps to multiple Item types (like text, shapes, etc.). Breaks a
    lot of stuff atm, but is most importantly backwards compatible with old
    project files. Also the TextItem is pretty rudamentary but serves as a
    proof of concept.
    
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- src/item.py
+++ src/item.py
@@ -4,20 +4,34 @@ import uuid
 from matplotlib import pyplot
 
 
-class Item:
-    def __init__(self, parent, xdata, ydata, name=""):
-        config = parent.preferences.config
+class ItemBase:
+    def __init__(self, config, name="", color=None, selected=True):
         self.key: str = str(uuid.uuid4())
         self.name = name
+        self.color = color
+        self.selected = selected
         self.plot_y_position = config["plot_y_position"]
         self.plot_x_position = config["plot_x_position"]
-        self.selected = True
-        self.color = None
+        self.xlabel = ""
+        self.ylabel = ""
+
+
+class Item(ItemBase):
+    def __init__(self, parent, xdata, ydata, name=""):
+        config = parent.preferences.config
+        super().__init__(config, name)
         self.xdata = xdata
         self.ydata = ydata
         self.linestyle = pyplot.rcParams["lines.linestyle"]
         self.linewidth = float(pyplot.rcParams["lines.linewidth"])
         self.markerstyle = pyplot.rcParams["lines.marker"]
         self.markersize = float(pyplot.rcParams["lines.markersize"])
-        self.xlabel = ""
-        self.ylabel = ""
+
+
+class TextItem(ItemBase):
+    def __init__(self, parent, x_anchor, y_anchor, text, name=""):
+        config = parent.preferences.config
+        super().__init__(config, name)
+        self.x_anchor = x_anchor
+        self.y_anchor = y_anchor
+        self.text = text
