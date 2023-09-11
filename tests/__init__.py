7c00a1c6bcf674d158429b4e56d7a0f7a4fe6c52
Sjoerd Stendahl <sjoerd1993@gmail.com>
2023-09-11 09:05:11 +0200

    Specify module imports in tests
    Fix import specification in tests
    
    remove main.py
    
    Linting
    
    Fix linting

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+"""Initialize testing module, specify used module versions"""
+import gi
+
+gi.require_version("Adw", "1")
+gi.require_version("Gdk", "4.0")
+gi.require_version("Graphs", "1")
+gi.require_version("Gtk", "4.0")
