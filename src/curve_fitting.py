5204200e58e4a1eb5c8a2774a1edc1bfc85ac316
Sjoerd Stendahl <sjoerd1993@gmail.com>
2023-09-15 17:20:09 +0200

    Fix issues
    linting

--- src/curve_fitting.py
+++ src/curve_fitting.py
@@ -9,31 +9,41 @@ from graphs.canvas import Canvas
 from graphs.data import Data
 from graphs.item import DataItem
 
+import matplotlib
 import numpy
 
 from scipy.optimize import curve_fit
 
 
-@Gtk.Template(resource_path="/se/sjoerd/Graphs/ui/curve_fitting.ui")
-class CurveFittingWindow(Adw.Window):
+class CurveFittingWindow(Graphs.CurveFittingTool):
     __gtype_name__ = "GraphsCurveFittingWindow"
-    equation = Gtk.Template.Child()
-    fitting_params = Gtk.Template.Child()
-    toast_overlay = Gtk.Template.Child()
-    text_view = Gtk.Template.Child()
+    confirm_button = GObject.Property(type=Gtk.Button)
+    equation = GObject.Property(type=Adw.EntryRow)
+    fitting_params = GObject.Property(type=Gtk.Box)
+    text_view = GObject.Property(type=Gtk.TextView)
+    toast_overlay = GObject.Property(type=Adw.ToastOverlay)
 
     def __init__(self, application, item):
+        figure_settings = \
+            Graphs.FigureSettings.new(
+                application.get_settings().get_child("figure"))
         super().__init__(
             application=application, transient_for=application.get_window(),
+            figure_settings=figure_settings,
         )
+        self.equation = self.get_equation()
         ui.bind_values_to_settings(
             self.get_application().get_settings("curve-fitting"), self)
+        self.get_confirm_button().connect("clicked", self.add_fit)
         canvas = Canvas(application)
         self.param = []
         self.sigma = []
         self.r2 = 0
 
-        self.equation.connect("notify::text", self.on_equation_change)
+        for prop in dir(figure_settings.props):
+            if prop not in ["use_custom_style", "custom_style"]:
+                figure_settings.bind_property(prop, canvas, prop, 1 | 2)
+        self.get_equation().connect("notify::text", self.on_equation_change)
         self.fitting_parameters = FittingParameterContainer(application)
 
         for var in self.get_free_variables():
@@ -56,16 +66,41 @@ class CurveFittingWindow(Adw.Window):
         canvas.props.items = [self.fitted_curve, self.data_curve]
 
         # Scale axis to the data to be fitted, set linear scale
-        for ax in canvas.axes:
-            ax.autoscale()
-            ax.yscale = "linear"
-            ax.xscale = "linear"
-        canvas.highlight_enabled = False
+
         self.canvas = canvas
         self.fit_curve()
         self.set_entry_rows()
-        self.toast_overlay.set_child(canvas)
+        self.get_toast_overlay().set_child(canvas)
+        self.set_canvas()
         self.present()
+
+    def set_canvas(self):
+        self.canvas.axis.yscale = "linear"
+        self.canvas.axis.xscale = "linear"
+        self.canvas.highlight_enabled = False
+        figure_settings = self.get_figure_settings()
+
+        xdata = list(self.data_curve.xdata)
+        ydata = list(self.data_curve.ydata)
+        xdata_fit = list(self.fitted_curve.xdata)
+        ydata_fit = list(self.fitted_curve.ydata)
+        upper_bound = list(self.upper_bound)
+        lower_bound = list(self.lower_bound)
+
+        ymax = max(ydata + ydata_fit + upper_bound)
+        ymin = min(ydata + ydata_fit + lower_bound)
+        xmax = max(xdata + xdata_fit)
+        xmin = min(xdata + xdata_fit)
+
+        xmin -= 0.015 * (xmax - xmin)
+        xmax += 0.015 * (xmax - xmin)
+        ymin -= 0.05 * (ymax - ymin)
+        ymax += 0.05 * (ymax - ymin)
+
+        figure_settings.set_property("min_left", ymin)
+        figure_settings.set_property("max_left", ymax)
+        figure_settings.set_property("min_bottom", xmin)
+        figure_settings.set_property("max_bottom", xmax)
 
     def get_free_variables(self):
         return re.findall(
@@ -90,7 +125,7 @@ class CurveFittingWindow(Adw.Window):
             except ValueError:
                 return False
 
-        for index, row in enumerate(self.fitting_params):
+        for index, row in enumerate(self.get_fitting_params()):
             param_entries = entry
 
             # Get the FittingParameterEntry class corresponding to the entry
@@ -126,21 +161,22 @@ class CurveFittingWindow(Adw.Window):
             buffer_string += f" (± {sigma})"
         buffer_string += f"\n\nSum of R²: {self.r2}"
 
-        self.text_view.get_buffer().set_text(buffer_string)
+        self.get_text_view().get_buffer().set_text(buffer_string)
         bold_tag = Gtk.TextTag(weight=700)
-        self.text_view.get_buffer().get_tag_table().add(bold_tag)
+        self.get_text_view().get_buffer().get_tag_table().add(bold_tag)
 
-        start_iter = self.text_view.get_buffer().get_start_iter()
-        end_iter = self.text_view.get_buffer().get_start_iter()
+        start_iter = self.get_text_view().get_buffer().get_start_iter()
+        end_iter = self.get_text_view().get_buffer().get_start_iter()
 
         # Highlight first word
         while not end_iter.ends_word() and not end_iter.ends_sentence():
             end_iter.forward_char()
-        self.text_view.get_buffer().apply_tag(bold_tag, start_iter, end_iter)
+        self.get_text_view().get_buffer().apply_tag(
+            bold_tag, start_iter, end_iter)
 
     @property
     def equation_string(self):
-        return utilities.preprocess(str(self.equation.get_text()))
+        return utilities.preprocess(str(self.get_equation().get_text()))
 
     def fit_curve(self):
         def _get_equation_name(equation_name, values):
@@ -169,11 +205,12 @@ class CurveFittingWindow(Adw.Window):
         ydata = [function(x, *self.param) for x in xdata]
 
         name = _get_equation_name(
-            str(self.equation.get_text()), self.param)
+            str(self.get_equation().get_text()), self.param)
         self.fitted_curve.set_name(f"Y = {name}")
         self.fitted_curve.ydata, self.fitted_curve.xdata = (ydata, xdata)
         self.get_confidence(function)
         self.set_results()
+        self.set_canvas()
         return True
 
     def get_confidence(self, function):
@@ -186,17 +223,19 @@ class CurveFittingWindow(Adw.Window):
         self.r2 = 1 - (ss_res / ss_sum)
 
         # Get confidence band
-        upper_bound = \
-            function(self.fitted_curve.xdata, *(self.param + self.sigma))
-        lower_bound = \
-            function(self.fitted_curve.xdata, *(self.param - self.sigma))
+        self.upper_bound = \
+            list(function(self.fitted_curve.xdata, *(self.param + self.sigma)))
+        self.lower_bound = \
+            list(function(self.fitted_curve.xdata, *(self.param - self.sigma)))
+        for collection in self.canvas.axis.collections:
+            if isinstance(collection, matplotlib.collections.PolyCollection):
+                collection.remove()
         self.canvas.axis.fill_between(self.fitted_curve.xdata,
-                                      lower_bound,
-                                      upper_bound,
+                                      self.lower_bound,
+                                      self.upper_bound,
                                       color=self.canvas.rubberband_fill_color,
                                       alpha=0.15)
 
-    @Gtk.Template.Callback()
     def add_fit(self, _widget):
         """Add fitted data to the items in the main application"""
         self.get_application().get_data().add_items([DataItem.new(
@@ -206,11 +245,12 @@ class CurveFittingWindow(Adw.Window):
         self.destroy()
 
     def set_entry_rows(self):
-        while self.fitting_params.get_last_child() is not None:
-            self.fitting_params.remove(self.fitting_params.get_last_child())
+        while self.get_fitting_params().get_last_child() is not None:
+            self.get_fitting_params().remove(
+                self.get_fitting_params().get_last_child())
 
         for arg in self.get_free_variables():
-            self.fitting_params.append(FittingParameterEntry(self, arg))
+            self.get_fitting_params().append(FittingParameterEntry(self, arg))
 
 
 class FittingParameterContainer(Data):
