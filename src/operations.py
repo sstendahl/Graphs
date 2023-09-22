a70f34f70567bffc752a7e4739060aafb4cc1a23
Sjoerd Stendahl <sjoerd1993@gmail.com>
2023-09-22 15:50:00 +0200

    Remove buggy inverse shifting

--- src/operations.py
+++ src/operations.py
@@ -201,9 +201,6 @@ def shift(item, xdata, ydata, left_scale, right_scale, items):
         if item.get_uuid() == item_.get_uuid():
             if scale == 1:  # Log scaling
                 new_ydata = [value * 10 ** shift_value_log for value in ydata]
-            elif scale == 4:
-                new_ydata = \
-                    [1/(1/value + shift_value_linear) for value in ydata]
             else:
                 new_ydata = [value + shift_value_linear for value in ydata]
             return xdata, new_ydata, False, False
