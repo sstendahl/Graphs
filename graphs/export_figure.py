e083234f52cffe17fdcceb9a7559a3c2d6215771
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2024-03-28 23:59:53 +0100

    Improve Export Figure handling
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- graphs/export_figure.py
+++ graphs/export_figure.py
@@ -1,12 +1,21 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 """Module for exporting the figure."""
 import contextlib
-from gettext import gettext as _
-from pathlib import Path
+from gettext import gettext as _, pgettext as C_
 
 from gi.repository import Adw, GLib, GObject, Gio, Graphs, Gtk
 
 from graphs import actions, file_io, ui, utilities
+
+_FILE_SUFFIXES = (
+    ("eps", ),  # Encapsulated Postscript
+    ("jpeg", "jpg"),  # Joint Photographic Experts Group
+    ("pdf", ),  # Portable Document Format
+    ("png", ),  # Portable Network Graphics
+    ("ps", ),  # Postscript
+    ("svg", "svgz"),  # Scalable Vector Graphics
+    ("webp", ),  # WebP Image Format
+)
 
 
 @Gtk.Template(resource_path="/se/sjoerd/Graphs/ui/export_figure.ui")
@@ -18,22 +27,10 @@ class ExportFigureDialog(Adw.Dialog):
     file_format = Gtk.Template.Child()
     transparent = Gtk.Template.Child()
 
-    file_formats = GObject.Property(type=object)
     application = GObject.Property(type=Graphs.Application)
 
     def __init__(self, application: Graphs.Application):
-        self._canvas = application.get_window().get_canvas()
-        valid_formats = self._canvas.get_supported_filetypes_grouped()
-        valid_formats.pop("Tagged Image File Format")
-        valid_formats.pop("Raw RGBA bitmap")
-        valid_formats.pop("PGF code for LaTeX")
-        super().__init__(
-            application=application,
-            file_formats=valid_formats,
-        )
-        self.file_format.set_model(
-            Gtk.StringList.new(list(self.file_formats.keys())),
-        )
+        super().__init__(application=application)
         ui.bind_values_to_settings(
             application.get_settings_child("export-figure"),
             self,
@@ -51,16 +48,15 @@ class ExportFigureDialog(Adw.Dialog):
     @Gtk.Template.Callback()
     def on_accept(self, _button) -> None:
         """Export the figure."""
-        file_format = self.file_format.get_selected_item().get_string()
-        file_suffixes = self.props.file_formats[file_format]
-        filename = \
-            Path(self._canvas.get_default_filename()).stem
+        file_suffixes = _FILE_SUFFIXES[self.file_format.get_selected()]
+        window = self.props.application.get_window()
+        filename = C_("filename", "Exported Figure")
 
         def on_response(dialog, response):
             with contextlib.suppress(GLib.GError):
                 file = dialog.save_finish(response)
                 with file_io.open_wrapped(file, "wb") as wrapper:
-                    self._canvas.figure.savefig(
+                    window.get_canvas().figure.savefig(
                         wrapper,
                         format=file_suffixes[0],
                         dpi=int(self.dpi.get_value()),
@@ -79,13 +75,16 @@ class ExportFigureDialog(Adw.Dialog):
                     toast = Adw.Toast.new(_("Exported Figure"))
                     toast.set_button_label(_("Open Location"))
                     toast.set_action_name("app.open-file-location")
-                    self.props.application.get_window().add_toast(toast)
+                    window.add_toast(toast)
                     self.close()
 
         dialog = Gtk.FileDialog()
         dialog.set_initial_name(f"{filename}.{file_suffixes[0]}")
         dialog.set_accept_label(_("Export"))
         dialog.set_filters(
-            utilities.create_file_filters([(file_format, file_suffixes)]),
+            utilities.create_file_filters([(
+                self.file_format.get_selected_item().get_string(),
+                file_suffixes,
+            )]),
         )
-        dialog.save(self.props.application.get_window(), None, on_response)
+        dialog.save(window, None, on_response)
