2dabf83ea4b3defaf31da227e9c5920a1fd82f21
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2024-04-05 11:48:54 +0200

    update build & CI
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

---
+++ build-aux/build-help-html.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ninja $(ninja -t targets rule CUSTOM_COMMAND | grep -E "^help.*\.page\$")
+
+while read lang; do
+    mkdir -p help-out/$lang
+    yelp-build html help/$lang -o help-out/$lang
+done < ../help/LINGUAS
+
+mkdir help-out/C
+yelp-build html ../help/C -o help-out/C
