f2f90bf63447d5de4a3909f6aa849b7162ddf374
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2024-04-25 15:04:56 +0200

    change edit behaviour & refactor
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- graphs/item_box.py
+++ graphs/item_box.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 """UI representation of an Item."""
-from gettext import gettext as _
-
-from gi.repository import Adw, Gio, Graphs
+from gi.repository import Graphs
 
 from graphs.curve_fitting import CurveFittingDialog
 from graphs.edit_item import EditItemDialog
@@ -19,54 +17,33 @@ class ItemBox(Graphs.ItemBox):
             item=item,
             index=index,
         )
-        self.setup()
-        self.props.drop_target.connect("drop", self.on_dnd_drop)
+        self.setup(len(application.get_data()))
+        self.connect("edit-request", self._on_edit_request)
+        self.connect("delete-request", self._on_delete_request)
+        self.connect("curve-fitting-request", self._on_curve_fitting_request)
+        self.connect(
+            "position-change-request", self._on_position_change_request,
+        )
 
-        action_list = ["edit", "delete", "curve_fitting"]
-        if self.props.index > 0:
-            action_list += ["move_up"]
-        if self.props.index + 1 < len(self.props.application.get_data()):
-            action_list += ["move_down"]
-        action_group = Gio.SimpleActionGroup()
+    @staticmethod
+    def _on_edit_request(self) -> None:
+        """Show Edit Item Dialog."""
+        EditItemDialog(self.props.application, self.props.item)
 
-        # Set the action group for the widget
-        for name in action_list:
-            action = Gio.SimpleAction.new(name, None)
-            action.connect("activate", getattr(self, f"{name}"))
-            action_group.add_action(action)
-        self.insert_action_group("item_box", action_group)
+    @staticmethod
+    def _on_delete_request(self) -> None:
+        """Delete Item."""
+        self.props.application.get_data().delete_items([self.props.item])
 
-    def _change_position(self, source_index: int) -> None:
-        data = self.props.application.get_data()
-        data.change_position(self.props.index, source_index)
-        data.add_history_state()
-        data.add_view_history_state()
-
-    def on_dnd_drop(self, _drop_target, value: int, _x, _y) -> None:
-        """Handle dropped data."""
-        self._change_position(value)
-
-    def curve_fitting(self, _action, _shortcut) -> None:
+    @staticmethod
+    def _on_curve_fitting_request(self) -> None:
         """Open Curve Fitting dialog."""
         CurveFittingDialog(self.props.application, self.props.item)
 
-    def move_up(self, _action, _shortcut) -> None:
-        """Move item up in hirarchy."""
-        self._change_position(self.props.index - 1)
-
-    def move_down(self, _action, _shortcut) -> None:
-        """Move item down in hirarchy."""
-        self._change_position(self.props.index + 1)
-
-    def delete(self, _action, _shortcut) -> None:
-        """Delete Item."""
-        name = self.props.item.get_name()
-        self.props.application.get_data().delete_items([self.props.item])
-        toast = Adw.Toast.new(_("Deleted {name}").format(name=name))
-        toast.set_button_label("Undo")
-        toast.set_action_name("app.undo")
-        self.props.application.get_window().add_toast(toast)
-
-    def edit(self, _action, _shortcut) -> None:
-        """Show Edit Item Dialog."""
-        EditItemDialog(self.props.application, self.props.item)
+    @staticmethod
+    def _on_position_change_request(self, index) -> None:
+        """Change Item position."""
+        data = self.props.application.get_data()
+        data.change_position(self.props.index, index)
+        data.add_history_state()
+        data.add_view_history_state()
