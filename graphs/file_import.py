ca3e12a1e99aed807ffc362bff7352ddbc5bd496
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2024-08-04 15:17:58 +0200

    refactor
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- graphs/file_import.py
+++ graphs/file_import.py
@@ -32,61 +32,32 @@ def import_from_files(
     present at /se/sjoerd/Graphs/import-params, a Window will be shown,
     giving the option to configure them.
     """
+    settings = application.get_settings_child("import-params")
     import_dict = {mode: [] for mode in _IMPORT_MODES.keys()}
     for file in files:
         import_dict[_guess_import_mode(file)].append(file)
-    modes = []
-    for mode, files in import_dict.items():
-        if files:
-            modes.append(mode)
-    configurable_modes = []
-    settings = application.get_settings_child("import-params")
-    for mode in settings.list_children():
-        if mode in modes:
-            configurable_modes.append(mode)
-    if configurable_modes:
+    modes = [mode for mode in settings.list_children() if import_dict[mode]]
 
-        def on_accept(_dialog):
-            _import_from_files(
-                application,
-                settings,
-                configurable_modes,
-                import_dict,
-            )
+    def do_import(_dialog):
+        items = []
+        style_manager = application.get_figure_style_manager()
+        style = style_manager.get_selected_style_params()
+        for mode, files in import_dict.items():
+            callback = getattr(parse_file, "import_from_" + mode)
+            params = settings.get_child(mode) if mode in modes else None
+            for file in files:
+                try:
+                    items.extend(callback(params, style, file))
+                except ParseError as error:
+                    application.get_window().add_toast_string(error.message)
+                    continue
+        application.get_data().add_items(items, style_manager)
 
-        dialog = Graphs.ImportDialog.new(application, configurable_modes)
-        dialog.connect("accept", on_accept)
+    if modes:
+        dialog = Graphs.ImportDialog.new(application, modes)
+        dialog.connect("accept", do_import)
     else:
-        _import_from_files(
-            application,
-            settings,
-            configurable_modes,
-            import_dict,
-        )
-
-
-def _import_from_files(
-    application: Graphs.Application,
-    settings: Gio.Settings,
-    configurable_modes: list[str],
-    import_dict: dict,
-):
-    items = []
-    style = application.get_figure_style_manager().get_selected_style_params()
-    for mode, files in import_dict.items():
-        callback = getattr(parse_file, "import_from_" + mode)
-        params = settings.get_child(mode) if mode in configurable_modes \
-            else None
-        for file in files:
-            try:
-                items.extend(callback(params, style, file))
-            except ParseError as error:
-                application.get_window().add_toast_string(error.message)
-                continue
-    application.get_data().add_items(
-        items,
-        application.get_figure_style_manager(),
-    )
+        do_import(None)
 
 
 def _guess_import_mode(file: Gio.File) -> str:
