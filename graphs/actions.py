f0432a7fc0d0bd03cdcb66c523b1794db2c1867c
Sjoerd Stendahl <sstendahl@gnome.org>
2024-09-05 04:28:39 +0000

    Remove duplicate method

--- graphs/actions.py
+++ graphs/actions.py
@@ -22,52 +22,6 @@ def on_action_invoked(application: Graphs.Application, name: str) -> None:
     getattr(sys.modules[__name__], name + "_action")(application)
 
 
-def add_equation_action(application: Graphs.Application) -> None:
-    """Add data from an equation."""
-
-    def on_accept(_dialog, name):
-        settings = application.get_settings_child("add-equation")
-        try:
-            x_start = utilities.string_to_float(settings.get_string("x-start"))
-            x_stop = utilities.string_to_float(settings.get_string("x-stop"))
-            step_size = utilities.string_to_float(
-                settings.get_string("step-size"),
-            )
-            datapoints = int(abs(x_start - x_stop) / step_size) + 1
-            xdata = numpy.ndarray.tolist(
-                numpy.linspace(x_start, x_stop, datapoints),
-            )
-            equation = utilities.preprocess(settings.get_string("equation"))
-            ydata = numpy.ndarray.tolist(
-                numexpr.evaluate(equation + " + x*0", local_dict={"x": xdata}),
-            )
-            if name == "":
-                name = f"Y = {settings.get_string('equation')}"
-            style_manager = application.get_figure_style_manager()
-            application.get_data().add_items(
-                [
-                    DataItem.new(
-                        style_manager.get_selected_style_params(),
-                        xdata,
-                        ydata,
-                        name=name,
-                    ),
-                ],
-                style_manager,
-            )
-            return ""
-        except ValueError as error:
-            return str(error)
-        except (NameError, SyntaxError, TypeError, KeyError) as exception:
-            message = _("{error} - Unable to add data from equation")
-            msg = message.format(error=exception.__class__.__name__)
-            logging.exception(msg)
-            return msg
-
-    dialog = Graphs.AddEquationDialog.new(application)
-    dialog.connect("accept", on_accept)
-
-
 def export_figure_action(application: Graphs.Application) -> None:
     """Export Figure."""
 
