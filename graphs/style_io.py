b72cb1e5def76104cce601ca927229be1ba321bf
Christoph Matthias Kohnen <christoph.kohnen@disroot.org>
2024-10-17 12:20:55 +0200

    fix linting
    Signed-off-by: Christoph Matthias Kohnen <christoph.kohnen@disroot.org>

--- graphs/style_io.py
+++ graphs/style_io.py
@@ -59,10 +59,10 @@ def parse(file: Gio.File, validate: RcParams = None) -> (RcParams, str):
                 line = line[9:]
             else:
                 graphs_param = False
-            if line_number == 2:
-                # legacy support for names at second line
-                if graphs_params["name"] is None and line[:2] == "# ":
-                    graphs_params["name"] = line[2:]
+            # legacy support for names at second line
+            if line_number == 2 and graphs_params["name"] is None \
+                    and line[:2] == "# ":
+                graphs_params["name"] = line[2:]
             line = cbook._strip_comment(line)
             if not line:
                 continue
