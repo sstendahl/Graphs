83d8fb04534f573d260ca4e9a7288b5317e695b3
Christoph Matthias Kohnen <mail@cmkohnen.de>
2026-02-07 14:27:08 +0100

    remove redundant figure style manager reference
    Signed-off-by: Christoph Matthias Kohnen <mail@cmkohnen.de>

--- graphs/application.py
+++ graphs/application.py
@@ -32,8 +32,7 @@ class PythonApplication(Graphs.Application):
         # We need to keep references as per
         # https://bugzilla.gnome.org/show_bug.cgi?id=687522
         PythonHelper(self)
-        self._figure_style_manager = StyleManager(self)
-        self.props.figure_style_manager = self._figure_style_manager
+        self.props.figure_style_manager = StyleManager(self)
         self._data_importer = DataImporter(self)
         self.props.data_importer = self._data_importer
 
