db6ae53ca6a770215d531cbfa860ccee8795a5fc
Christoph Matthias Kohnen <mail@cmkohnen.de>
2026-03-16 15:40:07 +0100

    cache light and dark system style

--- graphs/styles.py
+++ graphs/styles.py
@@ -28,6 +28,12 @@ def _generate_preview(params: Tuple[RcParams, dict]) -> Gdk.Texture:
     return Gdk.Texture.new_from_bytes(GLib.Bytes.new(buffer.getvalue()))
 
 
+def _params_for_bundled_style(name: str) -> tuple[RcParams, dict]:
+    filename = Graphs.filename_from_stylename(name)
+    uri = "resource:///se/sjoerd/Graphs/styles/" + filename
+    return style_io.parse(Gio.File.new_for_uri(uri))
+
+
 class StyleManager(Graphs.StyleManager):
     """
     Main Style Manager.
@@ -41,20 +47,22 @@ class StyleManager(Graphs.StyleManager):
     def __init__(self):
         # Check for Ubuntu
         gtk_theme = Gtk.Settings.get_default().get_property("gtk-theme-name")
-        self._system_style_name = "Yaru" \
+        system_style_name = "Yaru" \
             if "SNAP" in os.environ \
             and gtk_theme.lower().startswith("yaru") \
             else "Adwaita"
         super().__init__()
         self.connect("style-request", self._on_style_request)
         self.connect("create-style-request", self._on_create_style_request)
-        Adw.StyleManager.get_default().connect(
-            "notify",
-            self._update_system_style,
-        )
+        Adw.StyleManager.get_default().connect("notify", self._on_system_style)
 
-        self._update_system_style()
-        self.setup(self._system_style_name.lower())
+        self._system_style_light_params = \
+            _params_for_bundled_style(system_style_name)
+        self._system_style_dark_params = \
+            _params_for_bundled_style(system_style_name + " Dark")
+
+        self._on_system_style()
+        self.setup(system_style_name.lower())
 
     @staticmethod
     def _on_style_request(self, file: Gio.File) -> Graphs.Style:
@@ -81,21 +89,16 @@ class StyleManager(Graphs.StyleManager):
 
     def get_system_style_params(self) -> Tuple[RcParams, dict]:
         """Get the system style properties."""
-        return self._system_style_params
-
-    def _update_system_style(self, *_args) -> None:
-        system_style = self._system_style_name
         if Adw.StyleManager.get_default().get_dark():
-            system_style += " Dark"
-        filename = Graphs.filename_from_stylename(system_style)
-        self._system_style_params = style_io.parse(
-            Gio.File.new_for_uri(
-                "resource:///se/sjoerd/Graphs/styles/" + filename,
-            ),
-        )
+            return self._system_style_dark_params
+        else:
+            return self._system_style_light_params
+
+    def _on_system_style(self, *_args) -> None:
+        params = self.get_system_style_params()
         css = CSS_TEMPLATE.format(
-            background_color=self._system_style_params[0]["figure.facecolor"],
-            color=self._system_style_params[0]["text.color"],
+            background_color=params[0]["figure.facecolor"],
+            color=params[0]["text.color"],
         )
         self.props.css_provider.load_from_string(css)
 
@@ -109,7 +112,7 @@ class StyleManager(Graphs.StyleManager):
         """Copy a style."""
         style_params, graphs_params = style_io.parse(
             template.get_file(),
-            self._system_style_params,
+            self.get_system_style_params(),
         )
         graphs_params["name"] = new_name
         style_io.write(destination, style_params, graphs_params)
